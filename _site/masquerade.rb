a718767b7c37d574b412436caef3546daf62025d
Ryan Boucher <ryan.boucher@distributedlife.com>
2011-07-29 22:05:51 +1000

    added cromulent certification link

--- _site/masquerade.rb
+++ _site/masquerade.rb
@@ -6,7 +6,7 @@ set :public, Proc.new { File.join(root, "_site") }
 # This before filter ensures that your pages are only ever served 
 # once (per deploy) by Sinatra, and then by Varnish after that
 before do
-  response.headers['Cache-Control'] = 'public, max-age=500000' # 1 week
+  response.headers['Cache-Control'] = 'public, max-age=86400'
 end
 
 get '/' do
